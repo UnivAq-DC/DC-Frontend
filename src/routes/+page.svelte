f7deda8b0464eededaad9500936561296db76514
Specy <specy.dev@gmail.com>
2022-09-10 15:10:55 +0200

    Added sveltekit template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<div>
+    <h1>
+       Welcome to 4Lizzi
+    </h1>
+</div>
+
+<style lang='scss'>
+    :global(body, html){
+        width: 100%;
+        height: 100%;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+    }
+    div{
+        h1{
+            animation: woohoo infinite 1s alternate;
+            transform-origin: right;
+            font-weight: bold;
+            font-family: Arial;
+        }
+    }
+
+    @keyframes woohoo{
+        0%{
+            color: red;
+            transform: rotate(0) translateX(0);
+        }
+        50%{
+            color: yellow;
+            transform: rotate(180deg) translateX(100px) skew(50deg);
+        }
+        100%{
+            color: blue;
+            transform: rotate(0deg) translateX(50px);
+        }
+    }
+</style>
